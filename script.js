3c35055b3680ace184d07ede3797319efc154b5a
Ziad Hamama <ziadhamama123@gmail.com>
2025-05-12 22:01:07 -0230

    Initial commit - personal portfolio website

---
+++ script.js
@@ -0,0 +1,31 @@
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+};
+
+function openEmail() {
+    window.location.href = "mailto:ziadhamama123@gmail.com?body=Hi Ziad,";
+}
+
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    let params = {
+        name: document.getElementById('name').value,
+        email: document.getElementById('email').value,
+        message: document.getElementById('message').value
+    };
+
+    // Send email using EmailJS
+    emailjs.send('service_ouz5zrj', 'template_b2h615h', params)
+        .then(() => {
+            alert('Your message has been sent successfully!');
+            document.getElementById('contact-form').reset(); 
+        })
+        .catch(() => {
+            alert('Oops! Something went wrong. Please try again.');
+        });
+});
